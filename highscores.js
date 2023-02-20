ceacdf0a81bb54af80c9a0550b8c505dbb5b0078
Aoamos <opeyemiakanni4@gmail.com>
2023-02-20 12:05:25 +0100

    Working

---
+++ highscores.js
@@ -0,0 +1,6 @@
+const highSCoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScores.map(score => {
+    console.log(score);
+});
