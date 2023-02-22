e447c3b7de187b03cb8c87c54361043be26ac8b5
Aoamos <opeyemiakanni4@gmail.com>
2023-02-22 15:02:45 +0100

    Finished

--- highscores.js
+++ highscores.js
@@ -1,8 +1,8 @@
-const highSCoresList = document.getElementById('highScoresList');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highSCoresList.innerHTML = highScores
+highScoresList.innerHTML = highScores
     .map(score => {
-        return `<li class= "high-score">${score.name} - ${score.score}</li>`;
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
     })
     .join("");
